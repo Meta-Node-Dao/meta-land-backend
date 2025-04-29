5108f7d8e7c95dfa3f52db8749e126239da43bb3
lc3091 <309138912@qq.com>
2025-04-29 10:16:06 +0800

    update init sql

--- hack/migrate/20220328_0_428/upgrade.sql
+++
@@ -1,3 +0,0 @@
-create unique index startup_token_contract_index
-	on startup (token_contract_address);
-
--- hack/migrate/20220612_0_429/upgrade.sql
+++
@@ -1,2 +0,0 @@
-alter table `startup_wallet` drop index `startup_wallet_startup_id_wallet_address_uindex` ;
-alter table `startup_wallet` drop index `startup_wallet_startup_id_wallet_name_uindex` ;
